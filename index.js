1120a77681de6d925ed75e4275eb74c19788e9e2
LourencoDev <51246899+LourencoDev@users.noreply.github.com>
2020-12-30 18:39:16 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
-var express = require("express"),
-      app = express();
+const express = require("express");
+const app = express();
 
 var connection = null;
-app.get("/", function (req, res) {
+app.get("/", (req, res) => {
   res.send("Welcome to Shippable");
 });
 
-app.listen(3000, function () {
-  console.log('Express listening on port 3000');
+app.listen(3000, (error) => {
+  if(error)
+        console.log("Error: " + error);
+  else
+      console.log('Express listening on port 3000');
 });
